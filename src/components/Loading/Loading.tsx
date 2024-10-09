82c8b73599c3fccfc1be770ffedbbb849faf8d49
Marcus Felix <marcus.felix@gmail.com>
2024-10-09 20:05:45 +0200

    add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,6 @@
+import { IconRotateClockwise } from "@tabler/icons-react";
+import styles from "./Loading.module.css";
+
+export default function Loading() {
+  return <IconRotateClockwise className={styles.spinner}/>
+}
